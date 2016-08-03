046b893e1fa1ba54e8923d37e37db8465d967aca
cristianbretti <cristian.osoriobretti@gmail.com>
2016-08-03 14:14:00 +0200

    New working login activity

--- app/src/main/java/cristianosoriobretti/powerhourwithfriends/LoginActivity.java
+++ app/src/main/java/cristianosoriobretti/powerhourwithfriends/LoginActivity.java
@@ -18,17 +18,11 @@ import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-import com.spotify.sdk.android.authentication.AuthenticationClient;
-import com.spotify.sdk.android.authentication.AuthenticationRequest;
-import com.spotify.sdk.android.authentication.AuthenticationResponse;
-import com.spotify.sdk.android.player.ConnectionStateCallback;
 import java.util.ArrayList;
 
-public class LoginActivity extends Activity implements ConnectionStateCallback {
+public class LoginActivity extends Activity {
 
 
-    private static final String CLIENT_ID = "dc83b9c7f6ab47c299c90a43edc62d18";
-    private static final String REDIRECT_URI = "power-hour-with-friends-login://callback";
 
     private TextView textViewUser;
     private String token;
@@ -43,75 +37,11 @@ public class LoginActivity extends Activity implements ConnectionStateCallback {
         layoutRow = (LinearLayout) findViewById(R.id.layoutRow);
         relView = (RelativeLayout) findViewById(R.id.mainView);
 
-        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
-                AuthenticationResponse.Type.TOKEN,
-                REDIRECT_URI);
-        builder.setScopes(new String[]{"user-read-private", "streaming", "playlist-read-private"});
-        AuthenticationRequest request = builder.build();
-
-
-        AuthenticationClient.openLoginInBrowser(this, request);
-
+        Intent intent = getIntent();
+        user = (User) intent.getParcelableExtra("user");
+        populateScrollView();
     }
 
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-
-        Uri uri = intent.getData();
-        if (uri != null) {
-            AuthenticationResponse response = AuthenticationResponse.fromUri(uri);
-
-            switch (response.getType()) {
-                // Response was successful and contains auth token
-                case TOKEN:
-                    Log.d("App", "TOKEN");
-                    token = response.getAccessToken();
-                    JsonHandler handler = new JsonHandler();
-                    user = handler.createUser(token);
-                    textViewUser.setText(user.getUserName() + " Playlists");
-                    populateScrollView();
-                    break;
-
-                // Auth flow returned an error
-                case ERROR:
-                    // Handle error response
-                    Log.d("App", "ERROR");
-                    break;
-
-                // Most likely auth flow was cancelled
-                default:
-                    // Handle other cases
-                    Log.d("App", "default");
-            }
-        }
-    }
-
-    @Override
-    public void onLoggedIn() {
-        Log.d("MainActivity", "User logged in");
-    }
-
-    @Override
-    public void onLoggedOut() {
-        Log.d("MainActivity", "User logged out");
-        System.out.println("Kom hir");
-    }
-
-    @Override
-    public void onLoginFailed(Throwable error) {
-        Log.d("MainActivity", "Login failed");
-    }
-
-    @Override
-    public void onTemporaryError() {
-
-    }
-
-    @Override
-    public void onConnectionMessage(String s) {
-
-    }
 
     private void populateScrollView(){
         ArrayList<Playlist> list = user.getListOfPlaylists();
